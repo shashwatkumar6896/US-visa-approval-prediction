10278775eaabbe4b7d9a2465c92a516644d4bda3
shashwatkumar6896 <shashwatkumar6896@gmail.com>
2024-03-16 00:49:58 +0530

    folder structure added

--- template.py
+++ template.py
@@ -13,7 +13,7 @@ list_of_files = [
     f"{project_name}/components/model_trainer.py",
     f"{project_name}/components/model_evaluation.py",
     f"{project_name}/components/model_pusher.py",
-    f"{project_name}/comfiguration/__init__.py",
+    f"{project_name}/configuration/__init__.py",
     f"{project_name}/constants/__init__.py",
     f"{project_name}/entity/__init__.py",
     f"{project_name}/entity/config_entity.py",
@@ -33,6 +33,7 @@ list_of_files = [
     "setup.py",
     "config/model.yamal",
     "config/schema.yaml",
+    "test.py"
 
 
 
